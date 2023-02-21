ba47ab41b5650a3480833237de559f2261ed3edf
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-02-21 13:38:46 +0000

    call Variables

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//create a weather board?
+//variables 
+const searchCity = document.querySelector('.searchCity')
+const weathersContainer = $("#weather-container");
+const searchButton = $(".searchButton");
+const queryURL = "";
+const apiKey = "bd5e378503939ddaee76f12ad7a97608";
+const getCurrentData = function (name, forecastData) {
+    return {
+      name: name,
+      temperature: forecastData.current.temp,
+      wind: forecastData.current.wind_speed,
+      humidity: forecastData.current.humidity,
+      uvi: forecastData.current.uvi,
+      date: getFormattedDate(forecastData.current.dt, "- dddd - DD/MM/YYYY - HH:mm"),
+      iconCode: forecastData.current.weather[0].icon,
+    };
+  };
+
+
+
+//Function call for forecast-data 
+
+
